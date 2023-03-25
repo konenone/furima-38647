6814e39a331faccf708b5d5364bd9e74666180d1
konenone <konenonemickey@gmail.com>
2023-03-25 19:05:22 +0900

    商品出品機能修正③

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -121,6 +121,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it '価格が半角数字以外だったら出品できない' do
+        @item.price = 'aaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
     end
   end
 end
