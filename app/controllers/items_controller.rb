d4a27be1ffca4c9344ddd50ce052a97ab7b8facc
konenone <konenonemickey@gmail.com>
2023-03-31 20:22:36 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+  before_action :set_item, only: [:edit, :update]
+
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -22,11 +25,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    
+  end
+
+  
+def update
+    @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path(@item)
+  else
+    render :edit
+  end
+end
+
 
 private
 def item_params
   params.require(:item).permit(:name, :image, :introduction, :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
 end                                      
 
+def set_item
+  @item = Item.find(params[:id])
+end
 
 end
