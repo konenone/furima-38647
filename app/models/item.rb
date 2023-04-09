1a384050c2edb39a857a0ee915e57c585460ffeb
konenone <konenonemickey@gmail.com>
2023-04-09 21:14:56 +0900

    全体修正①

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,7 +24,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer:true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  with_options numericality: { other_than: 0 } do
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :shipping_area_id
     validates :status_id
